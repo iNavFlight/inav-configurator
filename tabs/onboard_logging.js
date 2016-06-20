faead1c7105cce66ddca8f53cd0f68291261576b
Konstantin Sharlaimov <konstantin.sharlaimov@gmail.com>
2016-06-21 07:59:08 +1000

    Enable blackbox config access for INAV 1.2+ (#21)

--- tabs/onboard_logging.js
+++ tabs/onboard_logging.js
@@ -4,8 +4,8 @@ var
     sdcardTimer;
 
 TABS.onboard_logging = {
-    available: false
 };
+
 TABS.onboard_logging.initialize = function (callback) {
     var 
         self = this,
@@ -17,28 +17,16 @@ TABS.onboard_logging.initialize = function (callback) {
     }
 
     if (CONFIGURATOR.connectionValid) {
-        // Blackbox was introduced in 1.5.0, dataflash API was introduced in 1.8.0, BLACKBOX/SDCARD MSP APIs in 1.11.0
-        TABS.onboard_logging.available = semver.gte(CONFIG.flightControllerVersion, "1.5.0");
-        
-        if (!TABS.onboard_logging.available) {
-            load_html();
-            return;
-        }
-        
         MSP.send_message(MSP_codes.MSP_BF_CONFIG, false, false, function() {
-            if (semver.gte(CONFIG.flightControllerVersion, "1.8.0")) {
-                MSP.send_message(MSP_codes.MSP_DATAFLASH_SUMMARY, false, false, function() {
-                    if (semver.gte(CONFIG.flightControllerVersion, "1.11.0")) {
-                        MSP.send_message(MSP_codes.MSP_SDCARD_SUMMARY, false, false, function() {
-                            MSP.send_message(MSP_codes.MSP_BLACKBOX_CONFIG, false, false, load_html);
-                        });
-                    } else {
-                        load_html();
-                    }
-                });
-            } else {
-                load_html();
-            }
+            MSP.send_message(MSP_codes.MSP_DATAFLASH_SUMMARY, false, false, function() {
+                if (semver.gte(CONFIG.flightControllerVersion, "1.2.0")) {
+                    MSP.send_message(MSP_codes.MSP_SDCARD_SUMMARY, false, false, function() {
+                        MSP.send_message(MSP_codes.MSP_BLACKBOX_CONFIG, false, false, load_html);
+                    });
+                } else {
+                    load_html();
+                }
+            });
         });
     }
     
