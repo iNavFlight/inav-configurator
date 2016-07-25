42cd6f00a92b014ebe63e042aab51d32cb1d41e4
Martin Budden <mjbudden@gmail.com>
2016-07-25 08:17:46 +0100

    Fixed NRF24 and SOFTSPI feature bits to match iNav

--- tabs/configuration.js
+++ tabs/configuration.js
@@ -156,8 +156,8 @@ TABS.configuration.initialize = function (callback, scrollPosition) {
 
         if (semver.gte(CONFIG.apiVersion, "1.21.0")) {
             features.push(
-                {bit: 22, group: 'rxMode', mode: 'group', name: 'RX_NRF24', haveTip: true},
-                {bit: 23, group: 'other', name: 'SOFTSPI'}
+                {bit: 26, group: 'rxMode', mode: 'group', name: 'RX_NRF24', haveTip: true},
+                {bit: 27, group: 'other', name: 'SOFTSPI'}
             );
         }
 
