748f72c81c79659bb8222af50504b2985fec31c2
Marcin Baliniak <marcin@baliniak.pl>
2017-07-04 17:12:37 +0200

    New GP module type MTK

--- build/script.js
+++ build/script.js
@@ -11505,6 +11505,10 @@ var FC = {
             data.push('UBLOX7')
         }
 
+        if (semver.gte(CONFIG.flightControllerVersion, "1.7.2")) {
+            data.push('MTK')
+        }
+
         return data;
     },
     getGpsBaudRates: function () {
