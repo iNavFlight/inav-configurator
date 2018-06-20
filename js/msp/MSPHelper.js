1de1f75093134ec7b57443a964949f78e584a5b1
Alberto García Hierro <alberto@garciahierro.com>
2018-06-20 15:58:24 +0100

    Handle MSPV2_INAV_SET_MISC in MSPHelper.js
    Fixes "unknown MSP code" warning

--- js/msp/MSPHelper.js
+++ js/msp/MSPHelper.js
@@ -373,6 +373,9 @@ var mspHelper = (function (gui) {
                 offset += 4;
                 MISC.battery_capacity_unit = (data.getUint8(offset++) ? 'mWh' : 'mAh');
                 break;
+            case MSPV2_INAV_SET_MISC:
+                console.log('MISC Configuration saved (MSPv2)');
+                break;
             case MSPCodes.MSPV2_INAV_BATTERY_CONFIG:
                 BATTERY_CONFIG.vbatscale = data.getUint16(offset, true);
                 offset += 2;
