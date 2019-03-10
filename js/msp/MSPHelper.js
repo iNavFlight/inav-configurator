6306f53da64e731218fcf6708b375f6f228b6f11
Pawel Spychalski (DzikuVx) <pspychalski@gmail.com>
2019-03-10 12:07:30 +0100

    Switch to MSP2_INAV_SERVO_MIXER when possible

--- js/msp/MSPHelper.js
+++ js/msp/MSPHelper.js
@@ -520,6 +520,22 @@ var mspHelper = (function (gui) {
                 SERVO_RULES.cleanup();
 
                 break;
+            case MSPCodes.MSP2_INAV_SERVO_MIXER:
+                SERVO_RULES.flush();
+                if (data.byteLength % 6 === 0) {
+                    for (i = 0; i < data.byteLength; i += 6) {
+                        SERVO_RULES.put(new ServoMixRule(
+                            data.getInt8(i),
+                            data.getInt8(i + 1),
+                            data.getInt16(i + 2, true),
+                            data.getInt8(i + 4),
+                            data.getInt8(i + 5)
+                        ));
+                    }
+                }
+                SERVO_RULES.cleanup();
+
+                break;
 
             case MSPCodes.MSP_SET_SERVO_MIX_RULE:
                 console.log("Servo mix saved");
@@ -3115,7 +3131,11 @@ var mspHelper = (function (gui) {
     };
 
     self.loadServoMixRules = function (callback) {
-        MSP.send_message(MSPCodes.MSP_SERVO_MIX_RULES, false, false, callback);
+        if (semver.gte(CONFIG.flightControllerVersion, "2.2.0")) {
+            MSP.send_message(MSPCodes.MSP2_INAV_SERVO_MIXER, false, false, callback);
+        } else {
+            MSP.send_message(MSPCodes.MSP_SERVO_MIX_RULES, false, false, callback);
+        }
     };
 
     self.loadMotorMixRules = function (callback) {
