150aa6916b682ac2aa8b4882b1d75286c8271874
Michel Pastor <shellixyz@users.noreply.github.com>
2019-08-25 02:14:22 +0200

    Add SPL006 barometer (#823)

--- js/fc.js
+++ js/fc.js
@@ -986,7 +986,9 @@ var FC = {
         return ["NONE", "AUTO", "HMC5883", "AK8975", "GPSMAG", "MAG3110", "AK8963", "IST8310", "QMC5883", "MPU9250", "IST8308", "LIS3MDL", "FAKE"];
     },
     getBarometerNames: function () {
-        if (semver.gte(CONFIG.flightControllerVersion, "2.0.0")) {
+        if (semver.gte(CONFIG.flightControllerVersion, "2.3.0")) {
+            return ["NONE", "AUTO", "BMP085", "MS5611", "BMP280", "MS5607", "LPS25H", "SPL006", "FAKE"];
+        } else if (semver.gte(CONFIG.flightControllerVersion, "2.0.0")) {
             return ["NONE", "AUTO", "BMP085", "MS5611", "BMP280", "MS5607", "LPS25H", "FAKE"];
         } else if (semver.gte(CONFIG.flightControllerVersion, "1.6.2")) {
             return ["NONE", "AUTO", "BMP085", "MS5611", "BMP280", "MS5607", "FAKE"];
