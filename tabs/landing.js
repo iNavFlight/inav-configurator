bb39007de4d086e75d63db4d0955adfbc1fd783e
Pawel Spychalski (DzikuVx) <pspychalski@gmail.com>
2019-11-21 22:29:23 +0100

    Cleanup landing page

--- tabs/landing.js
+++ tabs/landing.js
@@ -1,9 +1,8 @@
 'use strict';
-/*global $*/
+/*global $,TABS,GUI*/
 
 TABS.landing = {};
 TABS.landing.initialize = function (callback) {
-    var self = this;
 
     if (GUI.active_tab != 'landing') {
         GUI.active_tab = 'landing';
@@ -14,28 +13,8 @@ TABS.landing.initialize = function (callback) {
         // translate to user-selected language
         localize();
 
-        // load changelog content
-        $('#changelog .log').load('./changelog.html');
-
         $('div.welcome a, div.sponsors a').click(function () {
             googleAnalytics.sendEvent('ExternalUrls', 'Click', $(this).prop('href'));
-        });
-
-        /** changelog trigger **/
-        $("#changelog_toggle").on('click', function() {
-            var state = $(this).data('state2');
-            if (state) {
-                $("#changelog").animate({right: -245}, 200, function () {
-                    $("#content").removeClass('log_open');
-                    });
-                state = false;
-            } else {
-                $("#changelog").animate({right: 0}, 200);
-                $("#content").addClass('log_open');
-                state = true;
-            }
-            $(this).text(state ? 'Close' : 'Changelog');
-            $(this).data('state2', state);
         });
 
         GUI.content_ready(callback);
