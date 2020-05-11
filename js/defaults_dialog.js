5a2639667e69fddf1f0f15bb1f1baec88ec4960c
Pawel Spychalski (DzikuVx) <pspychalski@gmail.com>
2020-05-11 14:23:01 +0200

    Lower D-gains on MR defaults

--- js/defaults_dialog.js
+++ js/defaults_dialog.js
@@ -140,7 +140,7 @@ helper.defaultsDialog = (function() {
                 },
                 {
                     key: "mc_d_pitch",
-                    value: 28
+                    value: 25
                 },
                 {
                     key: "mc_p_roll",
@@ -152,7 +152,7 @@ helper.defaultsDialog = (function() {
                 },
                 {
                     key: "mc_d_roll",
-                    value: 28
+                    value: 23
                 },
                 {
                     key: "mc_p_yaw",
