67c0bc3108ad546e6f2b645f49015612a90c30e2
Andrew Young <yngndrw@gmail.com>
2020-11-19 01:05:15 +0000

    Removed mixer wizard validation as this no-longer makes sense

--- tabs/mixer.js
+++ tabs/mixer.js
@@ -283,32 +283,7 @@ TABS.mixer.initialize = function (callback, scrollPosition) {
             chrome.i18n.getMessage("mixerWizardModalTitle")
         );
 
-        function validateMixerWizard() {
-            let errorCount = 0;
-            for (let i = 0; i < 4; i++) {
-                const $elements = $('[data-motor] option:selected[id=' + i + ']'),
-                    assignedRulesCount = $elements.length;
-
-                if (assignedRulesCount != 1) {
-                    errorCount++;
-                    $elements.closest('tr').addClass("red-background");
-                } else {
-                    $elements.closest('tr').removeClass("red-background");
-                }
-            }
-
-            return (errorCount == 0);
-        }
-
-        $(".wizard-motor-select").change(validateMixerWizard);
-
         $("#wizard-execute-button").click(function () {
-
-            // Validate mixer settings
-            if (!validateMixerWizard()) {
-                return;
-            }
-
             MOTOR_RULES.flush();
 
             for (let i = 0; i < 4; i++) {
