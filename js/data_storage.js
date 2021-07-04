a623f231dc9ad23422061ac2a16b40fbb6b19d7c
Pawel Spychalski (DzikuVx) <pspychalski@gmail.com>
2021-07-04 12:10:45 +0200

    Version bump

--- js/data_storage.js
+++ js/data_storage.js
@@ -3,7 +3,7 @@
 var CONFIGURATOR = {
      // all versions are specified and compared using semantic versioning http://semver.org/
     'minfirmwareVersionAccepted': '3.0.0',
-    'maxFirmwareVersionAccepted': '3.2.0', // Condition is < (lt) so we accept all in 3.0 branch
+    'maxFirmwareVersionAccepted': '3.1.0', // Condition is < (lt) so we accept all in 3.0 branch
     'connectionValid': false,
     'connectionValidCliOnly': false,
     'cliActive': false,
