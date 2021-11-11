d639a3e274c9ee56f1a66e6e8d598a40643c120b
breadoven <56191411+breadoven@users.noreply.github.com>
2021-11-11 08:42:16 +0000

    Update osd.js

--- tabs/osd.js
+++ tabs/osd.js
@@ -915,7 +915,7 @@ OSD.constants = {
                 {
                     name: 'MISSION INFO',
                     id: 129,
-                    min_version: '4.0.0'
+                    min_version: '4.0.0',
                     preview: 'M1/6>27WP'
                 },
                 {
