8214d1c79b8a289014b6200139d979aa6fe44741
Darren Lines <darren@darrenlines.uk>
2021-11-13 08:52:51 +0000

    Added soaring and wp planner to mode ordering

--- js/data_storage.js
+++ js/data_storage.js
@@ -3,7 +3,7 @@
 var CONFIGURATOR = {
      // all versions are specified and compared using semantic versioning http://semver.org/
     'minfirmwareVersionAccepted': '3.0.0',
-    'maxFirmwareVersionAccepted': '5.0.0', // Condition is < (lt) so we accept all in 3.1 branch
+    'maxFirmwareVersionAccepted': '5.0.0', // Condition is < (lt) so we accept all in 4.x branch
     'connectionValid': false,
     'connectionValidCliOnly': false,
     'cliActive': false,
