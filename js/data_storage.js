cc0d987cd9a24e27bf6ff0a6fb6829172091698e
Pawel Spychalski (DzikuVx) <pspychalski@gmail.com>
2022-01-25 09:13:03 +0100

    Change min and max version

--- js/data_storage.js
+++ js/data_storage.js
@@ -2,8 +2,8 @@
 
 var CONFIGURATOR = {
      // all versions are specified and compared using semantic versioning http://semver.org/
-    'minfirmwareVersionAccepted': '3.0.0',
-    'maxFirmwareVersionAccepted': '6.0.0', // Condition is < (lt) so we accept all in 4.x branch
+    'minfirmwareVersionAccepted': '4.1.0',
+    'maxFirmwareVersionAccepted': '5.0.0', // Condition is < (lt) so we accept all in 4.x branch
     'connectionValid': false,
     'connectionValidCliOnly': false,
     'cliActive': false,
