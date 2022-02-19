0f27abf733ef260f084b9ee5de9f1b63deecd8ed
Darren Lines <darren@darrenlines.uk>
2022-02-19 18:34:37 +0000

    Add highlighting for fields that change with profiles
    - Added field arrays for control and battery profiles.
    - Added highlight mechanism for **data-setting** fields
    - Changed battery and control profile drop down text to match the field colours.

--- js/settings.js
+++ js/settings.js
@@ -12,6 +12,14 @@ var Settings = (function () {
             var settingName = input.data('setting');
             var inputUnit = input.data('unit');
 
+            if (FC.isBatteryProfileParameter(settingName)) {
+                input.css("background-color","#fef2d5");
+            }
+
+            if (FC.isControlProfileParameter(settingName)) {
+                input.css("background-color","#d5ebfe");
+            }
+
             return mspHelper.getSetting(settingName).then(function (s) {
                 // Check if the input declares a parent
                 // to be hidden in case of the setting not being available.
