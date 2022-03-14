e6ed93f69b23c690397ea3998a441513009bebd8
Pawel Spychalski (DzikuVx) <pspychalski@gmail.com>
2022-03-14 14:28:07 +0100

    Apply defaults confirmation

--- tabs/pid_tuning.js
+++ tabs/pid_tuning.js
@@ -135,18 +135,21 @@ TABS.pid_tuning.initialize = function (callback) {
         });
 
         $('#resetDefaults').on('click', function() {
-            mspHelper.setSetting("applied_defaults", 0, function() { 
-                mspHelper.saveToEeprom( function () {
-                    GUI.log(chrome.i18n.getMessage('configurationEepromSaved'));
 
-                    GUI.tab_switch_cleanup(function () {
-                        MSP.send_message(MSPCodes.MSP_SET_REBOOT, false, false, function () {
-                            GUI.log(chrome.i18n.getMessage('deviceRebooting'));
-                            GUI.handleReconnect();
+            if (confirm(chrome.i18n.getMessage('confirm_select_defaults'))) {
+                mspHelper.setSetting("applied_defaults", 0, function() { 
+                    mspHelper.saveToEeprom( function () {
+                        GUI.log(chrome.i18n.getMessage('configurationEepromSaved'));
+    
+                        GUI.tab_switch_cleanup(function () {
+                            MSP.send_message(MSPCodes.MSP_SET_REBOOT, false, false, function () {
+                                GUI.log(chrome.i18n.getMessage('deviceRebooting'));
+                                GUI.handleReconnect();
+                            });
                         });
                     });
                 });
-            });
+            }
         });
 
         pid_and_rc_to_form();
