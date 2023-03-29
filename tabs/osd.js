22144bb35d783e851694fcd5c8a21c45392647b2
Marcelo Bezerra <23555060+mmosca@users.noreply.github.com>
2023-03-29 19:36:50 +0200

    cleanup diff

--- tabs/osd.js
+++ tabs/osd.js
@@ -2144,13 +2144,13 @@ OSD.updateDisplaySize = function () {
     $('.preview').toggleClass('preview_avatar cut43_left', (video_type == 'AVATAR'))
     $('.third_right').toggleClass('preview_avatar_side', (video_type == 'AVATAR'))
     // -- DJI WTF
-    $('.third_left').toggleClass('preview_dji_hd_side', video_type == 'DJIWTF');
+    $('.third_left').toggleClass('preview_dji_hd_side', video_type == 'DJIWTF')
     $('.preview').toggleClass('preview_dji_hd cut43_left', video_type == 'DJIWTF')
     $('.third_right').toggleClass('preview_dji_hd_side', video_type == 'DJIWTF')
     // -- HD ZERO
-    $('.third_left').toggleClass('preview_hdzero_side', (video_type == 'HDZERO'));
-    $('.preview').toggleClass('preview_hdzero cut43_left', (video_type == 'HDZERO'));
-    $('.third_right').toggleClass('preview_hdzero_side', (video_type == 'HDZERO'));
+    $('.third_left').toggleClass('preview_hdzero_side', (video_type == 'HDZERO'))
+    $('.preview').toggleClass('preview_hdzero cut43_left', (video_type == 'HDZERO'))
+    $('.third_right').toggleClass('preview_hdzero_side', (video_type == 'HDZERO'))
     // -- BFHDCOMPAT
     $('.third_left').toggleClass('preview_bfhdcompat_side', (video_type == 'BFHDCOMPAT'))
     $('.preview').toggleClass('preview_bfhdcompat cut43_left', (video_type == 'BFHDCOMPAT'))
