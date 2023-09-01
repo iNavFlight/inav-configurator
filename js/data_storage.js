ef80a537c021fcf06ba43e2f0f04cc39163f15b4
Woga65 <woga@mytsa.de>
2023-09-01 02:52:09 +0200

    Bump versions of accepted releases

--- js/data_storage.js
+++ js/data_storage.js
@@ -2,7 +2,7 @@
 
 var CONFIGURATOR = {
      // all versions are specified and compared using semantic versioning http://semver.org/
-    'minfirmwareVersionAccepted': '6.1.1',
+    'minfirmwareVersionAccepted': '7.0.0',
     'maxFirmwareVersionAccepted': '8.0.0', // Condition is < (lt) so we accept all in 7.x branch
     'connectionValid': false,
     'connectionValidCliOnly': false,
