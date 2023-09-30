dbbe4cbe01a7e2a78fc5ccc45a95ded4b59b1913
Ray Morris <deepmagicbeginshere@gmail.com>
2023-09-29 22:54:23 -0500

    ipf yaw: tabs to spaces

--- js/fc.js
+++ js/fc.js
@@ -1271,7 +1271,7 @@ var FC = {
                     37: "Rangefinder [cm]",
                     38: "Active MixerProfile",
                     39: "MixerTransition Active",
-					40: "Yaw [deg]"
+                    40: "Yaw [deg]"
                 }
             },
             3: {
