7e190cc81683fadccc45cd6fdf7a3aef5edb5e4c
Roman Lut <11955117+RomanLut@users.noreply.github.com>
2024-05-06 14:30:03 +0200

    added holybro gnss

--- tabs/magnetometer.js
+++ tabs/magnetometer.js
@@ -726,7 +726,7 @@ TABS.magnetometer.initialize3D = function () {
     const loader = new THREE.GLTFLoader(manager);
 
     const magModelNames = ['xyz', 'ak8963c', 'ak8963n', 'ak8975', 'ak8975c', 'bn_880', 'diatone_mamba_m10_pro', 'flywoo_goku_m10_pro_v3', 'foxeer_m10q_120', 'foxeer_m10q_180', 'foxeer_m10q_250', 
-        'geprc_gep_m10_dq', 'gy271', 'gy273', 'hglrc_m100', 'qmc5883', 'ist8308', 'ist8310', 'lis3mdl', 
+        'geprc_gep_m10_dq', 'gy271', 'gy273', 'hglrc_m100', 'qmc5883', 'holybro_m9n_micro', 'holybro_m9n_micro', 'ist8308', 'ist8310', 'lis3mdl', 
         'mag3110', 'matek_m8q', 'matek_m9n', 'matek_m10q', 'mlx90393', 'mp9250', 'qmc5883', 'flywoo_goku_m10_pro_v3', 'ws_m181'];
     magModels = [];
 
