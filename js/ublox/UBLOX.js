dd3e10fd59d19ce9ecf1ba68b175e72dc3c01dd2
Marcelo Bezerra <23555060+mmosca@users.noreply.github.com>
2024-06-23 14:51:47 +0200

    Add new file

---
+++ js/ublox/UBLOX.js
@@ -0,0 +1,26 @@
+'use strict';
+
+const semver = require('semver');
+
+require('./../injected_methods');
+const { GUI } = require('./../gui');
+
+var ublox = (function () {
+    var self = {};
+    var assistnowOnline = null;
+    var assitnowOffline = null;
+    
+    self.init = function() {
+
+    };
+
+    self.loadAssitnowOffline = function(token) {
+
+    };
+
+    self.loadAssistnowOnline = function(token) {
+
+    }
+    
+});
+
