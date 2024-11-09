46ead5a70e187ed73c4a52cb7543b761de8506dd
Mr D - RC <darren@darrenlines.uk>
2024-11-09 09:55:00 +0000

    Update link to OSD symbols for custom elements

--- js/globalUpdates.js
+++ js/globalUpdates.js
@@ -17,6 +17,7 @@ var update = {
         if (CONFIGURATOR.connectionValid) {
             $('#logo .firmware_version').text(FC.CONFIG.flightControllerVersion + " [" + FC.CONFIG.target + "]");
             globalSettings.docsTreeLocation = 'https://github.com/iNavFlight/inav/blob/' + FC.CONFIG.flightControllerVersion + '/docs/';
+            globalSettings.configuratorTreeLocation = 'https://github.com/iNavFlight/inav-configurator/tree/' + FC.CONFIG.flightControllerVersion + '/';
 
             // If this is a master branch firmware, this will find a 404 as there is no tag tree. So default to master for docs.
             $.ajax({
