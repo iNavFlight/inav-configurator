80d2a6e756502f5defd72b10165d8fc6d450ae6c
Marcelo Bezerra <23555060+mmosca@users.noreply.github.com>
2025-01-29 22:30:07 +0100

    Some users are confused by the default artifact names.
    Clean things up a little bit.

--- forge.config.js
+++ forge.config.js
@@ -27,6 +27,9 @@ module.exports = {
         result.artifacts.forEach(artifact => {
           var artifactStr = artifact.toString();
           var newPath = path.join(path.dirname(artifactStr), baseName + path.extname(artifactStr));
+		  newPath.replace('win32_ia32', 'Win32');
+		  newPath.replace('win32_x64', 'Win64');
+		  newPath.replace('darwin', 'MacOS');
           fs.renameSync(artifactStr, newPath);
           console.log('Artifact: ' + newPath);
         });
