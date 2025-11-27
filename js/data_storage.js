a522ba55868e88176a6488e721e6550719b7e129
Lemontree-Softworks <lemontree-softworks@outlook.com>
2025-11-27 11:33:17 -0300

    Revoke test  min firmware version

--- js/data_storage.js
+++ js/data_storage.js
@@ -2,7 +2,7 @@
 
 var CONFIGURATOR = {
      // all versions are specified and compared using semantic versioning http://semver.org/
-    'minfirmwareVersionAccepted': '8.0.0',
+    'minfirmwareVersionAccepted': '9.0.0',
     'maxFirmwareVersionAccepted': '10.0.0', // Condition is < (lt) so we accept all in 8.x branch
     'connectionValid': false,
     'connectionValidCliOnly': false,
