8cd8eaab3122680eab927108d33a3f8424341bcf
error414 <error414@gmail.com>
2026-05-24 20:44:25 +0200

    get back accidentally deleted line

--- js/serial_backend.js
+++ js/serial_backend.js
@@ -301,6 +301,8 @@ var SerialBackend = (function () {
                     } else {
                         $(`#tabs ul.mode-connected .tab_setup a`).trigger('click');
                     }
+
+                    update.firmwareVersion();
                 });
             });
         });
