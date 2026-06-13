b555d6c2fcf240ed92a999f78c493f934a31ca10
error414 <error414@gmail.com>
2026-06-13 12:46:29 +0200

    terrain - dedicated OSD element

--- tabs/osd.js
+++ tabs/osd.js
@@ -142,6 +142,7 @@ SYM.PILOT_LOGO_SML_L = 0x1D5;
 SYM.PILOT_LOGO_SML_C = 0x1D6;
 SYM.PILOT_LOGO_SML_R = 0x1D7;
 SYM.MIN_GND_SPEED = 0xDE;
+SYM.TERRAIN_FOLLOWING = 0xFB;
 
 SYM.AH_AIRCRAFT0 = 0x1A2;
 SYM.AH_AIRCRAFT1 = 0x1A3;
@@ -1380,6 +1381,31 @@ OSD.constants = {
                                 return FONT.embed_dot('13.8') + FONT.symbol(SYM.DIST_NM);
                             default: // Metric
                                 return FONT.embed_dot('25.6') + FONT.symbol(SYM.DIST_KM);
+                        }
+                    }
+                },
+                {
+                    name: 'OSD_TERRAIN_AGL',
+                    id: 170,
+                    enabled: function() {
+                        return HARDWARE.capabilities.useTerrain;
+                    },
+                    preview: function(osd_data) {
+
+                        var s = '114';
+                        if (Settings.getInputValue('osd_decimals_altitude') == 4) {
+                            s += '3';
+                        } if (Settings.getInputValue('osd_decimals_altitude') == 5) {
+                            s += '38';
+                        }
+
+                        switch (OSD.data.preferences.units) {
+                            case 0: // Imperial
+                            case 3: // UK
+                            case 4: // GA
+                                return FONT.symbol(SYM.TERRAIN_FOLLOWING) + s + FONT.symbol(SYM.ALT_FT);
+                            default: // Metric
+                                return FONT.symbol(SYM.TERRAIN_FOLLOWING) + s + FONT.symbol(SYM.ALT_M);
                         }
                     }
                 }
@@ -3573,7 +3599,8 @@ HARDWARE.init = function() {
         useRx: false,
         useCRSF: false,
         useBaro: false,
-        usePitot: false
+        usePitot: false,
+        useTerrain: false,
     };
 };
 
@@ -3608,6 +3635,10 @@ HARDWARE.update = function(callback) {
                 }
             });
         });
+
+        mspHelper.getSetting("terrain_enabled").then(function(data) {
+            HARDWARE.capabilities.useTerrain = Boolean(data.value);
+        })
     });
 };
 
