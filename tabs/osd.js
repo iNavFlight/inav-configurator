14259173718e4f6d6af0eb62024a51ee849297dc
error414 <error414@gmail.com>
2026-07-12 19:30:29 +0200

    Revert "fix OSD Tab"
    This reverts commit 78a6b96580a4c8c8efad1b0603d24609fc0c2eee.

--- tabs/osd.js
+++ tabs/osd.js
@@ -2806,17 +2806,7 @@ OSD.GUI.preview = {
             position += overflows_line;
         }
 
-        var $positionInput = $('input.' + item_id + '.position');
-        if ($positionInput.length > 0) {
-            $positionInput.val(position).trigger('change');
-        } else {
-            // The element is hidden from the list by the search filter, so its
-            // position input doesn't exist in the DOM. Update the item directly.
-            var itemData = OSD.data.items[item_id];
-            itemData.position = position;
-            OSD.msp.helpers.calculate.coords(itemData);
-            OSD.GUI.saveItem(item);
-        }
+        $('input.' + item_id + '.position').val(position).trigger('change');
     }
 };
 
@@ -3518,11 +3508,6 @@ OSD.GUI.updateAll = function() {
         OSD.GUI.updateFields(event);
     });
     $('.supported').fadeIn();
-    // fadeIn() above matches the `.supported` toolbar too and stamps an inline
-    // `display: block` on it, which overrides `.content_toolbar { display: flex }`
-    // and stacks its buttons vertically. Clear the inline display and drop `hide`
-    // so the CSS flex layout applies.
-    $('.content_toolbar').css('display', '').removeClass('hide');
     OSD.GUI.updateVideoMode();
     OSD.GUI.updateUnits();
     OSD.GUI.updateFields();
